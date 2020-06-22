70a5954150e1d3eae54770a36792c0000ecceaa9
awesome <pupul31051999@gmail.com>
2020-06-22 21:58:10 +0530

    2nd commit

---
+++ src/main/java/com/example/MyApp/random_stuff/prime.java
@@ -0,0 +1,29 @@
+package com.example.MyApp.random_stuff;
+import java.util.Scanner;
+
+public class prime {
+    public static boolean is_prime(int n)
+    {
+        int k= (int) Math.sqrt(n);
+        int no_factors=0;
+        for(int i=1;i<=k;k++)
+        {
+            if(n%i==0)
+                no_factors++;
+        }
+        if(no_factors==2)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int val=sc.nextInt();
+        if(is_prime(val))
+            System.out.println("number is prime");
+        else
+            System.out.println("number is not prime");
+    }
+}
